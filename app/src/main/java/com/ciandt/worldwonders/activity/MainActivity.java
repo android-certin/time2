fa21a8b8c747b154f66bd1b451270450f38b1852
Anderson Ribeiro e Silva <anderonr@ciandt.com>
2015-08-21 16:08:31 -0300

    Utilizando final para Tag de log

--- app/src/main/java/com/ciandt/worldwonders/activity/MainActivity.java
+++ app/src/main/java/com/ciandt/worldwonders/activity/MainActivity.java
@@ -13,11 +13,13 @@ import com.ciandt.worldwonders.fragment.LoginFragment;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("MainActivity", "onCreate");
+        Log.i(TAG, "onCreate");
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
@@ -28,36 +30,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i("MainActivity", "onStart");
+        Log.i(TAG, "onStart");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i("MainActivity", "onRestart");
+        Log.i(TAG, "onRestart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i("MainActivity", "onResume");
+        Log.i(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i("MainActivity", "onPause");
+        Log.i(TAG, "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i("MainActivity", "onStop");
+        Log.i(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i("MainActivity", "onDestroy");
+        Log.i(TAG, "onDestroy");
     }
 }
