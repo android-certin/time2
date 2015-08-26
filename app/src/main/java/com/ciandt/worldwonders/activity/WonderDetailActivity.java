d8ed0966feb11a18710ded1acfcc86d204f26537
Wagner Peireira Gomes <wapegomes@gmail.com>
2015-08-26 12:39:13 -0300

    Adequação dela.

--- app/src/main/java/com/ciandt/worldwonders/activity/WonderDetailActivity.java
+++ app/src/main/java/com/ciandt/worldwonders/activity/WonderDetailActivity.java
@@ -1,12 +1,16 @@
 package com.ciandt.worldwonders.activity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ciandt.worldwonders.R;
+import com.ciandt.worldwonders.helpers.Helpers;
 import com.ciandt.worldwonders.model.Wonder;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by wgomes on 26/08/15.
@@ -22,7 +26,18 @@ public class WonderDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wonder_detail);
 
         TextView txDetailName = (TextView) findViewById(R.id.detail_name);
+        TextView txDetailDescription = (TextView) findViewById(R.id.detail_description);
+        ImageView imgDetail = (ImageView) findViewById(R.id.detail_image);
+
         txDetailName.setText(wonder.getName().toString());
+        txDetailDescription.setText(wonder.getDescription().toString());
+
+        String img = wonder.getPhoto().split("\\.")[0];
+
+        Picasso.with(this).load(Helpers.getRawResourceID(this, img))
+                .config(Bitmap.Config.RGB_565).centerCrop()
+                .into(imgDetail);
+
 
     }
 
