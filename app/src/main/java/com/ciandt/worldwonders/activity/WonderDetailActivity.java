18316c3cb327b00568fe8c8ca0af94ae80b2da09
enirsilvaferraz <enirsilvaferraz@gmail.com>
2015-08-26 15:26:54 -0300

    Menu

--- app/src/main/java/com/ciandt/worldwonders/activity/WonderDetailActivity.java
+++ app/src/main/java/com/ciandt/worldwonders/activity/WonderDetailActivity.java
@@ -90,7 +90,7 @@ public class WonderDetailActivity extends AppCompatActivity {
                 }
 
                 else {
-                    Task.
+                    //Task.
                 }
 
 
